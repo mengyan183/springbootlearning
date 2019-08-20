9de352672ce5969af4794eb20446fa90f26f77c9
xingguo <xingguo0828@gmail.com>
2019-08-20 12:10:35 +0800

    find judge

--- back-end/xcEduService01/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/AlgorithmTest.java
+++ back-end/xcEduService01/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/AlgorithmTest.java
@@ -3,11 +3,13 @@
  */
 package com.xuecheng.manage_cms.dao;
 
-import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.util.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -32,7 +34,8 @@ public class AlgorithmTest {
         if (N < 2) {
             return N;
         }
-        // 在 二维数组 的 第二个 值存在 , 第一个 值 不存在 的为 法官
+        // 入度为N-1;出度为0的 为法官
+        // 在 二维数组 的 第二个 值存在 , 第一个 值 不存在 的为 法官 候选人
         Set<Integer> xList = new HashSet<>(trust.length);
         Set<Integer> yList = new HashSet<>(trust.length);
         for (int[] ints : trust) {
@@ -47,16 +50,87 @@ public class AlgorithmTest {
         if (yList.size() == 0) {
             return -1;
         }
+        // 获取小镇上 所有 人
+        HashSet<Integer> allTownPeople = new HashSet<>();
+        allTownPeople.addAll(yList);
+        allTownPeople.addAll(xList);
         for (Integer candidate : yList){
             // 如果入度为N-1代表 除 本身外 , 信任关系中 包含其余人 的全部二维数组
             if(candidate != null){
-
+                //相信 该候选人 的 人
+                HashSet<Integer> candidateTrusted = new HashSet<>();
+                for (int[] ints : trust) {
+                    int x = ints[0];
+                    int y = ints[1];
+                    if (candidate.equals(y)) {
+                        candidateTrusted.add(x);
+                    }
+                }
+                HashSet<Integer> copyAllTownPeople = new HashSet<>(allTownPeople);
+                //小镇所有人减去所有相信该候选人的人 ;如果有剩余 且 剩余人数 为 1 且 剩余的人 为 该候选人 则 候选人 为 法官
+                copyAllTownPeople.removeAll(candidateTrusted);
+                if (!CollectionUtils.isEmpty(copyAllTownPeople) && copyAllTownPeople.size() == 1 && copyAllTownPeople.contains(candidate)) {
+                    return candidate;
+                }
             }
         }
-
         return -1;
     }
 
+    /**
+     * 入度为N-1;出度为0的 为法官
+     *
+     * @param N     人数
+     * @param trust 信任关系
+     * @return
+     */
+    public Integer findJudgeV2(int N, int[][] trust) {
+        if (N < 2) {
+            return N;
+        }
+        // 入度
+        HashMap<Integer, Integer> incomingDegree = new HashMap<>();
+        // 出度
+        HashMap<Integer, Integer> outDegree = new HashMap<>();
+        // 入度 - 出度 = N-1
+        for (int[] ints : trust) {
+            incomingDegree.put(ints[1], incomingDegree.getOrDefault(ints[1], 0) + 1);
+            outDegree.put(ints[0], outDegree.getOrDefault(ints[0], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : incomingDegree.entrySet()) {
+            if (entry.getValue() != null && entry.getValue().equals(N - 1) && !outDegree.containsKey(entry.getKey())) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 入度为N-1;出度为0的 为法官
+     *
+     * @param N     人数
+     * @param trust 信任关系
+     * @return
+     */
+    public Integer findJudgeV3(int N, int[][] trust) {
+        if (N < 2) {
+            return N;
+        }
+        int[] dValue = new int[N];
+        // 入度 - 出度 = N-1
+        for (int[] ints : trust) {
+            // 出度
+            dValue[ints[0]]--;
+            // 入度
+            dValue[ints[1]]++;
+        }
+        for (int i : dValue) {
+            if (i == N - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
     @Test
     public void test() {
         int n = 3;
