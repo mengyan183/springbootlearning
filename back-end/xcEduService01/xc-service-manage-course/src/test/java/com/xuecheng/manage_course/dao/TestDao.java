ab5c5693436ccecda3afefdf64ae1e7dc2a60b35
xingguo <xingguo0828@gmail.com>
2019-10-13 20:55:52 +0800

    分页查询

--- back-end/xcEduService01/xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
+++ back-end/xcEduService01/xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
@@ -1,6 +1,8 @@
 package com.xuecheng.manage_course.dao;
 
 import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.xuecheng.framework.domain.course.CourseBase;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -38,5 +40,13 @@ public class TestDao {
     public void testCourseMapper(){
         CourseBase courseBase = courseMapper.findCourseBaseById("402885816240d276016240f7e5000002");
         log.info(JSON.toJSONString(courseBase));
+    }
+
+    @Test
+    public void testCourseList(){
+        //将数据写入到 thredlocal 中 ，然后拦截之后执行的sql 语句 并 拼接 sql limit 参数 以及 count 查询（默认统计，可以手动设置配置文件关闭）
+        PageHelper.startPage(1,10);
+        Page<CourseBase> list = courseMapper.findList();
+        log.info(list.toString());
     }
 }
