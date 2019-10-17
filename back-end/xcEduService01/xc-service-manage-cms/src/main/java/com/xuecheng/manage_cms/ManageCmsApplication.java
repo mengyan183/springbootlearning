11ab904384970041d8c587addbf6c3077db8082a
xingguo <xingguo0828@gmail.com>
2019-10-17 16:37:07 +0800

    服务注册到 eureka

--- back-end/xcEduService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ back-end/xcEduService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -6,6 +6,8 @@ package com.xuecheng.manage_cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
  * @version 1.0
  * @create 2018-09-12 17:13
  **/
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
