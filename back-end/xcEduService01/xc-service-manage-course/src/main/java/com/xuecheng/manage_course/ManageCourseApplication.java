a3fa52e97b1ca21977299f69bc3a4d03a7375c66
xingguo <xingguo0828@gmail.com>
2019-10-17 20:37:12 +0800

    test ribbon

--- back-end/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
+++ back-end/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Administrator
@@ -21,5 +25,15 @@ import org.springframework.context.annotation.ComponentScan;
 public class ManageCourseApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ManageCourseApplication.class, args);
+    }
+
+    /**
+     * 声明resttemplate，将其交由spring context 管理
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
     }
 }
---
+++ back-end/xcEduService01/xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TetRibbon.java
@@ -0,0 +1,31 @@
+package com.xuecheng.manage_course.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class TetRibbon {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void testCmsPageList() {
+        //服务id
+        String serviceId = "XC-SERVICE-MANAGE-CMS";
+        for (int i = 0; i < 10; i++) {
+            //通过服务id调用
+            ResponseEntity<CmsPage> forEntity = restTemplate.getForEntity("http://" + serviceId + "/cms/page/get/5a754adf6abb500ad05688d9", CmsPage.class);
+            CmsPage cmsPage = forEntity.getBody();
+        }
+    }
+}
