368f118f8f910525d96c4f19fc8268af513ef31b
xingguo <xingguo0828@gmail.com>
2019-11-05 21:49:56 +0800

    ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console...

--- back-end/xcEduService01/xc-service-search/src/main/java/com/xuecheng/search/SearchApplication.java
+++ back-end/xcEduService01/xc-service-search/src/main/java/com/xuecheng/search/SearchApplication.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.xuecheng.search"})//扫描本项目下的所有类
 @ComponentScan(basePackages = {"com.xuecheng.framework"})//扫描common下的所有类
 //使用servicecomb
-//@EnableServiceComb
+@EnableServiceComb
 public class SearchApplication {
 
     public static void main(String[] args) throws Exception {
