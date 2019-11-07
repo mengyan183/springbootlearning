6c2e0c745826bc0feec59f08c1dfa02bf24f4cb3
xingguo <xingguo0828@gmail.com>
2019-11-07 22:14:51 +0800

    springcloud 接入cse 尚未成功

--- back-end/xcEduService01/xc-service-search/src/main/java/com/xuecheng/search/SearchApplication.java
+++ back-end/xcEduService01/xc-service-search/src/main/java/com/xuecheng/search/SearchApplication.java
@@ -1,22 +1,20 @@
 package com.xuecheng.search;
 
-import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Administrator
  * @version 1.0
  **/
-@SpringBootApplication()
+@SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.search")//扫描实体类
 @ComponentScan(basePackages = {"com.xuecheng.api"})//扫描接口
 @ComponentScan(basePackages = {"com.xuecheng.search"})//扫描本项目下的所有类
 @ComponentScan(basePackages = {"com.xuecheng.framework"})//扫描common下的所有类
-//使用servicecomb
-@EnableServiceComb
 public class SearchApplication {
 
     public static void main(String[] args) throws Exception {
